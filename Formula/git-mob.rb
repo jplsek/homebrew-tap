c5d12a52eebe6ae46653abb2b4dfdbe7254f6619
Jeremy Plsek <jeremyplsek@gmail.com>
2021-01-19 18:38:50 -0500

    git-mob 2.0.0

--- Formula/git-mob.rb
+++ Formula/git-mob.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitMob < Formula
   desc "CLI tool for including co-authors in commits"
   homepage "https://github.com/findmypast-oss/git-mob#readme"
-  url "https://registry.npmjs.org/git-mob/-/git-mob-1.1.0.tgz"
-  sha256 "1cb6f40afddf9a55681dece3b442b442d621c517f0f7e59058c464b0030678bd"
+  url "https://registry.npmjs.org/git-mob/-/git-mob-2.0.0.tgz"
+  sha256 "72bfb1538321c24e2f3be4fc78d2e3f1402722aa4434d3f03f2d2aada9327531"
 
   depends_on "node"
 
