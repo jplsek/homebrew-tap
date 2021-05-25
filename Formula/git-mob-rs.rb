f76e96e27d88e512023172ac2dfce2145959ec5e
Jeremy Plsek <jeremyplsek@gmail.com>
2021-05-24 20:08:05 -0400

    Update git-mob-rs to 0.1.2

--- Formula/git-mob-rs.rb
+++ Formula/git-mob-rs.rb
@@ -1,8 +1,8 @@
 class GitMobRs < Formula
   desc "CLI tool for including co-authors in commits"
   homepage "https://github.com/jplsek/git-mob-rs"
-  url "https://github.com/jplsek/git-mob-rs/releases/download/v0.1.1/git-mob-rs-0.1.1-macos.zip"
-  sha256 "a431bc2fb0651499ce0a784c90358124a077efc1cfadfabc1d8fd5729a31cec4"
+  url "https://github.com/jplsek/git-mob-rs/releases/download/v0.1.2/git-mob-rs-0.1.2-macos.zip"
+  sha256 "ad18fd24eb00331cecff6299b289292d93dba5e1bfa2ac0dd8d348e09da8480d"
   license "MIT"
 
   conflicts_with "git-duet/tap/git-duet", because: "git-duet also has a solo command"
