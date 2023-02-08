16037ff22a84725655bb657fa81900f0bd782964
Jeremy Plsek <jeremyplsek@gmail.com>
2023-02-07 20:20:03 -0500

    Update to git-mob-rs 0.3.0

--- Formula/git-mob-rs.rb
+++ Formula/git-mob-rs.rb
@@ -1,9 +1,11 @@
 class GitMobRs < Formula
   desc "CLI tool for including co-authors in commits"
   homepage "https://github.com/jplsek/git-mob-rs"
-  url "https://github.com/jplsek/git-mob-rs/releases/download/v0.2.0/git-mob-rs-macos-intel-0.2.0.zip"
-  sha256 "d26874a4e257cbcdc622c1eb32a36964d9c381dbb74959b3da50d1b771c0abd0"
+  url "https://github.com/jplsek/git-mob-rs/releases/download/v0.3.0/git-mob-rs-macos-intel-0.3.0.zip"
+  sha256 "e6d48d9aa8ee43af9be0b82507a5e499c64343f84d7f5008f86ed1a9a4980297"
   license "MIT"
+
+  depends_on "openssl"
 
   conflicts_with "git-duet/tap/git-duet", because: "git-duet also has a solo command"
   conflicts_with "jplsek/tap/git-mob", because: "git-mob uses the same commands"
@@ -15,6 +17,7 @@ class GitMobRs < Formula
     bin.install "git-add-coauthor"
     bin.install "git-edit-coauthor"
     bin.install "git-delete-coauthor"
+    bin.install "git-mob-print"
   end
 
   test do
